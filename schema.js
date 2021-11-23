fbd1229c2b3d5a1310294c6af2c87836a3c5ec9d
Robert Howton <robert.f.howton@gmail.com>
2021-11-23 15:39:26 -0500

    quell server functional

--- schema.js
+++ schema.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-
 const {
   GraphQLObjectType,
   GraphQLInt,
--- server.js
+++ server.js
@@ -3,18 +3,33 @@ const { graphqlHTTP } = require('express-graphql');
 const cors = require('cors');
 const schema = require('./schema');
 const path = require('path');
+const QuellCache = require('@quell/server');
 
 const app = express();
 
 // Allow cross-origin
 app.use(cors());
 
+app.use(express.json());
+
+const quellCache = new QuellCache(schema, 6379, 300000);
+
+// WARNING: if you get the error: "TypeError: Cannot read property '_fields' of undefined", 
+// insert the following code into line 483 of node_modules/quell/server/src/quell.js:
+// 
+// if (!schema.mutation) return null;
+
+// WARNING: Requires redis-server to be run in terminal on (default) port 6397 (should be handled by `npm run dev`)
+
 app.use(
   '/graphql',
-  graphqlHTTP({
-    schema,
-    graphiql: true
-  })
+  quellCache.query, (req, res) => {
+   return res.status(200).send(res.locals.queryResponse); 
+  }
+  // graphqlHTTP({
+  //   schema,
+  //   graphiql: true
+  // })
 );
 
 app.use(express.static('public'));
