196a243203dca00da8c9bd8439f850b98c71e6af
ccai89 <ccai89@gmail.com>
2021-11-27 22:01:50 -0500

    DARRRRRK mode

--- quell-extension/src/pages/Panel/Input/Editor.jsx
+++ quell-extension/src/pages/Panel/Input/Editor.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import { Controlled as CodeMirror } from 'react-codemirror2';
 import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
 
 const Editor = (props) => {
 
   const[defaultText, setText] = useState("Enter GraphQL query here\n\n\n\n");
-  
+
   return(
     <CodeMirror
+      id='thing'
       value={defaultText}
       options={{
+        theme: 'material',
         lineNumbers: true,
         lint:true,
         mode:'graphql'
@@ -17,6 +20,7 @@ const Editor = (props) => {
       onBeforeChange={(editor, data, value) => {
         setText({value}); console.log(value);
       }}
+      // sends Query to parent componet to be processed by 
       onChange={(editor, data, value) => {
         props.queriedText(value);
       }}
