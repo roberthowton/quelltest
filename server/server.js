461ab437de6c146ea28c0bd670c7f15b4b6160d6
Robert Howton <robert.f.howton@gmail.com>
2021-12-13 14:24:29 -0500

    server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const QuellCache = require('../Quell/quell-server/src/quell');
+// const QuellCache = require('../../quell/quell-server/src/quell');
+const QuellCache = require('../Quell/quell-server/src/quell')
 
 const PORT = 3000;
 
@@ -22,14 +23,23 @@ app.use(express.urlencoded({ extended: true }));
 
 app.options('/graphql', cors());
 app.use('/graphql', cors(), quellCache.query, (req, res) => {
-  console.log('request: ', req.originalUrl);
+  console.log(res.locals.queryResponse)
   return res.status(200).send(res.locals.queryResponse);
 });
 
 app.options('/redis', cors());
-app.use('/redis', cors(), quellCache.getStatsFromRedis, (req, res) => {
-  return res.status(200).send(res.locals.output);
-});
+app.use('/redis', ...quellCache.getRedisInfo({
+  getStats: true,
+  getKeys: true,
+  getValues: true
+}))
+
+// app.use('/redis', cors(), quellCache.getStatsFromRedis, quellCache.getRedisKeys, quellCache.getRedisValues, (req, res) => {
+//   res.locals.redisStats.keys = res.locals.keys;
+//   res.locals.redisStats.values = res.locals.values;
+//   // console.log('redis stats: ', res.locals.redisStats);
+//   return res.status(200).send(res.locals.redisStats);
+// });
 
 app.options('/clearCache', cors());
 app.get('/clearCache', cors(), quellCache.clearCache, (req, res) => {
